a5b2784f76b22c8b0cbd20987878714147bb0cee
Shawn Rieger <riegersn@gmail.com>
2017-04-21 12:18:24 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+SuicideGirls for Boxee Box
+=====================
+SuicideGirls app for the Boxee Box by Shawn Rieger (http://www.boxee.tv/) - SuicideGirls is a community that celebrates ALTERNATIVE BEAUTY and alternative culture from all over the world.
---
+++ suicidegirls/launch.py
@@ -0,0 +1,20 @@
+#
+# SuicideGirls
+# App launch script
+#
+# by Shawn Rieger / Boxee
+#
+
+import mc
+import sg
+
+reg = mc.GetApp().GetLocalConfig()
+
+if reg.GetValue("username") and reg.GetValue("password") and sg.check_authentication():
+	reg.SetValue("authenticated", "true")
+else:
+	reg.Reset("authenticated")
+	#if sg.login_sg(reg.GetValue('username'), reg.GetValue('password')):
+	#	reg.SetValue('authenticated', 'true')
+
+sg.quickLaunch(14000)
